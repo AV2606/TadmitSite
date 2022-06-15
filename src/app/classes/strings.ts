d9417fe04a6601a819112f9c17926e0b5e75c64b
Avichay Vaknin <79922086+AV2606@users.noreply.github.com>
2022-06-16 00:25:27 +0300

    version 0.0.3- added color animation to the home page and a description on every language and framework

--- src/app/classes/strings.ts
+++ src/app/classes/strings.ts
@@ -1,6 +1,14 @@
+import { bottom } from "@popperjs/core";
+
 export enum Languages{
     hebrew='hebrew',
     english='english'
+}
+export interface LangDesc {
+    // Used to display the top of the details div
+    Top:string;
+    //Used to display the bottom of the details div
+    Bottom:string;
 }
 export class Strings{
 
@@ -11,4 +19,62 @@ export class Strings{
     static readonly first_name= 'Avichay';//this.language==Languages.english? 'Avichay':this.notSupportedYetChar;
     static readonly last_name='Vaknin';
     static readonly shortDesc=`<strong>The program magician.</br>Full-stack.</strong>`;
+    static readonly Languages={
+        cspng:{
+        Top:"C# is a general-purpose, object-oriented programming language. It was developed by Microsoft and is used in a wide variety of applications.",
+        Bottom:".Net | WPF | UWP | WinForms | ASP | Task based multi-threading | Strong Typed | OOP | EF Core | GC"
+        }
+        , javapng:{
+        Top:"Java is a general-purpose computer programming language that is concurrent, class-based, object-oriented, and designed to be a platform for developing large, complex software systems.",
+        Bottom:" Android & Android Studio | iOS | Strong Typed | OOP | GC | JVM "
+        },
+        tspng:{
+        Top:"TypeScript is a JavaScript-based language with strict type checking and first-class functions. It is a superset of JavaScript, and is designed to be a drop-in replacement for JavaScript.",
+        Bottom:"JS Super set | Strong and weakly Typed | OOP | Functional programming  | DOM | Angular | React | Node.js | NPM"
+        },
+        htmlpng:{
+        Top:"HTML is the standard markup language for creating Web pages. It is used to create markup which is a text format for representing content on the World Wide Web.",
+        Bottom:"HTML5 | CSS3 | JS | DOM | V8"
+        },
+        jspng:{
+        Top:"JavaScript is a high-level, interpreted programming language. It is a language which is also characterized as dynamic, weakly typed, prototype-based and multi-parthic.",
+        Bottom:"ES6 | DOM | V8 | GC | Not Java | Prototype-based | Multi-parthic | Weakly Typed | Dynamic | Closures"
+        },
+        csspng:{
+        Top:"CSS is a style sheet language used for describing the presentation of a document written in a markup language.",
+        Bottom:"CSS3 | DOM | Declaration-based | Flexbox | Grid | Media Queries | Sass | Less | Bootstrap | Tailwindcss"
+        },
+        sqlpng:{
+        Top:"SQL is a programming language that is used for managing data in a database.",
+        Bottom:"SQL Server | SMSS | Queires | Stored Procedures | Data Types | Data Manipulation | Data Definition | Scheme | Primary-Key "
+        },
+        netpng:{
+        Top:".NET is a platform for creating applications and services for the Internet.",
+        Bottom:"CLR | IL | COM | C# | .Net Core | .Net Framework | Xamarin | Blazor | ASP.Net"
+        },
+        angularpng:{
+        Top:"Angular is a JavaScript framework for building single-page applications.",
+        Bottom:"Angular.js | Angular 13 | Virtual DOM | Templates | Data-Binding | js in html"
+        },
+        nodepng:{
+        Top:"Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine.",
+        Bottom:"V8 | JS | DOM | Single-threaded | Asynchronous | Event-based | Serve-side & Client-side"
+        },
+        entitypng:{
+        Top:"Entity Framework is a data access framework for Microsoft SQL Server.",
+        Bottom:"EF Core | EF 6 | .NET | C#"
+        },
+        vanillapng:{
+        Top:"Vanilla JS is a the 'base' framework for building user interfaces.",
+        Bottom:"Vanilla JS | JS | DOM | Single-threaded | Asynchronous | Event-based | Client-side"
+        },
+        androidstudiopng:{
+        Top:"Android Studio is a graphical user interface (GUI) tool for developing Android applications.",
+        Bottom:"Android Studio | Android | Java | JavaFX | Kotlin | Gradle | Android SDK | Google"
+        },
+        asppng:{
+        Top:"ASP.NET is a web application framework for building dynamic web sites and web applications.",
+        Bottom:"ASP.NET | C# | .Net Core | .Net Framework | Web Forms | Razor | MVC | Web API"
+        }
+    }
 }
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -8,71 +8,119 @@ import { RandomService } from 'src/app/services/random.service';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  
+  showDetail=false;
 
-  Strings=strings;
-  cspng:HTMLElement|null=null;
-  javapng:HTMLElement|null=null;
-  csspng:HTMLElement|null= null;
-  htmlpng:HTMLElement|null=null;
-  tspng:HTMLElement|null=null;
-  jspng:HTMLElement|null=null;
-  sqlpng:HTMLElement|null=null;
+  Strings = strings;
+  cspng: HTMLElement | null = null;
+  javapng: HTMLElement | null = null;
+  csspng: HTMLElement | null = null;
+  htmlpng: HTMLElement | null = null;
+  tspng: HTMLElement | null = null;
+  jspng: HTMLElement | null = null;
+  sqlpng: HTMLElement | null = null;
 
-  netpng: HTMLElement | null=null;
-  angularpng: HTMLElement | null=null;
-  androidstudiopng: HTMLElement | null=null;
-  nodepng: HTMLElement | null=null;
-  asppng: HTMLElement | null=null;
-  vanillapng: HTMLElement | null=null;
-  entitypng: HTMLElement | null=null;
+  netpng: HTMLElement | null = null;
+  angularpng: HTMLElement | null = null;
+  androidstudiopng: HTMLElement | null = null;
+  nodepng: HTMLElement | null = null;
+  asppng: HTMLElement | null = null;
+  vanillapng: HTMLElement | null = null;
+  entitypng: HTMLElement | null = null;
 
-  hoverup="hover";
+  langDesc: HTMLElement | null = null;
+  nameDropping: HTMLElement | null = null;
+  hoverup = "hover";
 
   animations: Promise<void>[] = this.getAnimations();
 
 
-  constructor(private Random:RandomService) {
-    
+  constructor(private Random: RandomService) {
+
   }
 
   ngOnInit(): void {
-    this.cspng=document.getElementById('cspng');
-    this.javapng=document.getElementById('javapng');
-    this.csspng=document.getElementById('csspng');
-    this.htmlpng=document.getElementById('htmlpng');
-    this.tspng=document.getElementById('tspng');
-    this.jspng=document.getElementById('jspng');
-    this.sqlpng=document.getElementById('sqlpng');
-    this.netpng=document.getElementById('netpng');
-    this.angularpng=document.getElementById('angularpng');
-    this.androidstudiopng=document.getElementById('androidstudiopng');
-    this.nodepng=document.getElementById('nodepng');
-    this.asppng=document.getElementById('asppng');
-    this.vanillapng=document.getElementById('vanillapng');
-    this.entitypng=document.getElementById('entitypng');
-    let langs=document.getElementById("langsContainer");
-    if(this.cspng!=null)
-      this.cspng.addEventListener("mouseover",()=>{
-        console.log("listened");
-        console.log(langs);
-        //this.hoverup="hover";
-        if(langs)
-        langs.classList.add("hover");
+    this.cspng = document.getElementById('cspng');
+    this.javapng = document.getElementById('javapng');
+    this.csspng = document.getElementById('csspng');
+    this.htmlpng = document.getElementById('htmlpng');
+    this.tspng = document.getElementById('tspng');
+    this.jspng = document.getElementById('jspng');
+    this.sqlpng = document.getElementById('sqlpng');
+    this.netpng = document.getElementById('netpng');
+    this.angularpng = document.getElementById('angularpng');
+    this.androidstudiopng = document.getElementById('androidstudiopng');
+    this.nodepng = document.getElementById('nodepng');
+    this.asppng = document.getElementById('asppng');
+    this.vanillapng = document.getElementById('vanillapng');
+    this.entitypng = document.getElementById('entitypng');
+    let langs = document.getElementById("langsContainer");
+    this.langDesc = document.getElementById("langsDesc");
+    this.nameDropping = document.getElementById("nameDropping");
+    if (this.cspng != null)
+      this.cspng.addEventListener("mouseover", () => {
+        if (langs)
+          langs.classList.add("hover");
       });
     this.animateInLangsAsync();
+    this.setGrayScale();
   }
-  async animateInLangsAsync(){
-    let anims:any[]=[];
-    let i=0;
-    while(this.animations.length>0){
-    let rnd=this.Random.getRandomInt(0,this.animations.length-1);
-    let o=null;
-    o=await this.animations.splice(rnd,1)[0];
-    anims.push(o);
+  onPicHover(id: string) {
+    let element = document.getElementById(id);
+    if (element != null) {
+      element.style.filter = "";
+      element.classList.add("hover");
+      if (this.langDesc != null && this.nameDropping != null){
+        let langs:any=this.Strings.Languages;
+        this.langDesc.textContent = langs[id]["Top"];
+        this.nameDropping.innerHTML = `Name Dropping: <br><br> `;
+        this.nameDropping.innerHTML += langs[id]["Bottom"];
+        this.showDetail=true;
+      }
+    }
+  }
+  setGrayScale() {
+    if (this.cspng != null)
+      this.cspng.style.filter = "grayscale(100%)";
+    if (this.javapng != null)
+      this.javapng.style.filter = "grayscale(100%)";
+    if (this.csspng != null)
+      this.csspng.style.filter = "grayscale(100%)";
+    if (this.htmlpng != null)
+      this.htmlpng.style.filter = "grayscale(100%)";
+    if (this.tspng != null)
+      this.tspng.style.filter = "grayscale(100%)";
+    if (this.jspng != null)
+      this.jspng.style.filter = "grayscale(100%)";
+    if (this.sqlpng != null)
+      this.sqlpng.style.filter = "grayscale(100%)";
+    if (this.netpng != null)
+      this.netpng.style.filter = "grayscale(100%)";
+    if (this.angularpng != null)
+      this.angularpng.style.filter = "grayscale(100%)";
+    if (this.androidstudiopng != null)
+      this.androidstudiopng.style.filter = "grayscale(100%)";
+    if (this.nodepng != null)
+      this.nodepng.style.filter = "grayscale(100%)";
+    if (this.asppng != null)
+      this.asppng.style.filter = "grayscale(100%)";
+    if (this.vanillapng != null)
+      this.vanillapng.style.filter = "grayscale(100%)";
+    if (this.entitypng != null)
+      this.entitypng.style.filter = "grayscale(100%)";
+  }
+  async animateInLangsAsync() {
+    let anims: any[] = [];
+    let i = 0;
+    while (this.animations.length > 0) {
+      let rnd = this.Random.getRandomInt(0, this.animations.length - 1);
+      let o = null;
+      o = await this.animations.splice(rnd, 1)[0];
+      anims.push(o);
     }
     for (const iterator of anims) {
       iterator();
-      await new Promise(resolve=>{setTimeout(resolve,250)});
+      await new Promise(resolve => { setTimeout(resolve, 250) });
     }
 
   }
@@ -80,77 +128,77 @@ export class MainComponent implements OnInit {
    * 
    * @param duration in milliseconds for the animation
    */
-  async animateInOneLangAsync(element:HTMLElement|null,duration:number){
-    if(element == null)
-        return;
+  async animateInOneLangAsync(element: HTMLElement | null, duration: number) {
+    if (element == null)
+      return;
     //the time it takes to animate 1% of the animation. the animation has 100 parts.
-    let percentInterval=duration/100;
-    for(let i=0;i<=100;i++){
-      await new Promise(resolve=>{
-        setTimeout(()=>{
-          element.style.opacity=""+(i/100);
+    let percentInterval = duration / 100;
+    for (let i = 0; i <= 100; i++) {
+      await new Promise(resolve => {
+        setTimeout(() => {
+          element.style.opacity = "" + (i / 100);
           resolve(0);
-        },percentInterval);
+        }, percentInterval);
       });
     }
   }
   getAnimations(): any[] {
-    let arr:any[]=[];
-    arr.push(async ()=>{
-    this.animateInOneLangAsync(this.cspng,1000);
-    await new Promise(resolve=>{setTimeout(resolve,500)});
+    let arr: any[] = [];
+    arr.push(async () => {
+      this.animateInOneLangAsync(this.cspng, 1000);
+      await new Promise(resolve => { setTimeout(resolve, 500) });
     });
-    arr.push(async()=>{
-    this.animateInOneLangAsync(this.javapng,1000);
-    await new Promise(resolve=>{setTimeout(resolve,500)});
+    arr.push(async () => {
+      this.animateInOneLangAsync(this.javapng, 1000);
+      await new Promise(resolve => { setTimeout(resolve, 500) });
     });
-    arr.push(async()=>{
-    this.animateInOneLangAsync(this.tspng,1000);
-    await new Promise(resolve=>{setTimeout(resolve,500)});
+    arr.push(async () => {
+      this.animateInOneLangAsync(this.tspng, 1000);
+      await new Promise(resolve => { setTimeout(resolve, 500) });
     });
-    arr.push(async()=>{
-    this.animateInOneLangAsync(this.jspng,1000);
-    await new Promise(resolve=>{setTimeout(resolve,500)});
+    arr.push(async () => {
+      this.animateInOneLangAsync(this.jspng, 1000);
+      await new Promise(resolve => { setTimeout(resolve, 500) });
     });
-    arr.push(async()=>{
-    this.animateInOneLangAsync(this.csspng,1000);
-    await new Promise(resolve=>{setTimeout(resolve,500)});
+    arr.push(async () => {
+      this.animateInOneLangAsync(this.csspng, 1000);
+      await new Promise(resolve => { setTimeout(resolve, 500) });
     });
-    arr.push(async()=>{
-    this.animateInOneLangAsync(this.htmlpng,1000);
-    await new Promise(resolve=>{setTimeout(resolve,500)});
+    arr.push(async () => {
+      this.animateInOneLangAsync(this.htmlpng, 1000);
+      await new Promise(resolve => { setTimeout(resolve, 500) });
     });
-    arr.push(async()=>{
-    this.animateInOneLangAsync(this.sqlpng,1000);
-    await new Promise(resolve=>{setTimeout(resolve,500)});
+    arr.push(async () => {
+      this.animateInOneLangAsync(this.sqlpng, 1000);
+      await new Promise(resolve => { setTimeout(resolve, 500) });
     });
-    arr.push(async()=>{
-    this.animateInOneLangAsync(this.netpng,1000);
-    await new Promise(resolve=>{setTimeout(resolve,500)});
+    arr.push(async () => {
+      this.animateInOneLangAsync(this.netpng, 1000);
+      await new Promise(resolve => { setTimeout(resolve, 500) });
     });
-    arr.push(async()=>{
-    this.animateInOneLangAsync(this.angularpng,1000);
-    await new Promise(resolve=>{setTimeout(resolve,500)});
+    arr.push(async () => {
+      this.animateInOneLangAsync(this.angularpng, 1000);
+      await new Promise(resolve => { setTimeout(resolve, 500) });
     });
-    arr.push(async()=>{
-    this.animateInOneLangAsync(this.androidstudiopng,1000);
-    await new Promise(resolve=>{setTimeout(resolve,500)});
+    arr.push(async () => {
+      this.animateInOneLangAsync(this.androidstudiopng, 1000);
+      await new Promise(resolve => { setTimeout(resolve, 500) });
     });
-    arr.push(async()=>{
-    this.animateInOneLangAsync(this.nodepng,1000);
-    await new Promise(resolve=>{setTimeout(resolve,500)});
+    arr.push(async () => {
+      this.animateInOneLangAsync(this.nodepng, 1000);
+      await new Promise(resolve => { setTimeout(resolve, 500) });
     });
-    arr.push(async()=>{
-    this.animateInOneLangAsync(this.asppng,1000);
-    await new Promise(resolve=>{setTimeout(resolve,500)});
+    arr.push(async () => {
+      this.animateInOneLangAsync(this.asppng, 1000);
+      await new Promise(resolve => { setTimeout(resolve, 500) });
     });
-    arr.push(async()=>{
-    this.animateInOneLangAsync(this.vanillapng,1000);
-    await new Promise(resolve=>{setTimeout(resolve,500)});
+    arr.push(async () => {
+      this.animateInOneLangAsync(this.vanillapng, 1000);
+      await new Promise(resolve => { setTimeout(resolve, 500) });
     });
-    arr.push(async()=>{
-    this.animateInOneLangAsync(this.entitypng,1000);
-    await new Promise(resolve=>{setTimeout(resolve,500)});
+    arr.push(async () => {
+      this.animateInOneLangAsync(this.entitypng, 1000);
+      await new Promise(resolve => { setTimeout(resolve, 500) });
     });
     return arr;
   }
